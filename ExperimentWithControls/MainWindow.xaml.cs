f9b2450917926f22d8aa119e82cc19c9429c100d
SpookyAction@Distance <jreland87@gmail.com>
2020-12-09 21:33:47 -0500

    zero flaws

--- ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/MainWindow.xaml.cs
@@ -25,22 +25,22 @@ namespace ExperimentWithControls
             InitializeComponent();
         }
 
-        private void numberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void NumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             number.Text = numberTextBox.Text;
         }
 
-        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        private void NumberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            e.Handled = !int.TryParse(e.Text, out int result);
+            e.Handled = !int.TryParse(e.Text, out _);
         }
 
-        private void smallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void SmallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             number.Text = smallSlider.Value.ToString("0");
         }
 
-        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void BigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             number.Text = bigSlider.Value.ToString("000-000-0000");
         }
@@ -53,7 +53,7 @@ namespace ExperimentWithControls
             }
         }
 
-        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (myListBox.SelectedItem is ListBoxItem listBoxItem)
             {
@@ -61,7 +61,7 @@ namespace ExperimentWithControls
             }
         }
 
-        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ReadOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
             {
@@ -69,7 +69,7 @@ namespace ExperimentWithControls
             }
         }
 
-        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void EditableComboBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (sender is ComboBox comboBox)
             {
